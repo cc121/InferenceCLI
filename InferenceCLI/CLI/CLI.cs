03514c46bef43078ca6dbb5bdec1b0d6eb8aabf8
cc121 <ccooper91@gatech.edu>
2024-05-31 17:48:47 -0600

    Add project files.

---
+++ InferenceCLI/CLI/CLI.cs
@@ -0,0 +1,56 @@
+﻿using System.CommandLine;
+
+namespace InferenceCLI.CLI
+{
+    internal class CLI
+    {
+        private readonly RootCommand rootCommand;
+        private Option<string> modelOption;
+        private Option<string> precisionOption;
+
+        public CLI() {
+            // Define the options
+            modelOption = new Option<string>(
+                "--model",
+                getDefaultValue: () => "Phi3-Mini-4K",
+                description: "The model to run inference with. Options are Phi3-Mini-4K and Phi3-Mini-128K.")
+                    .FromAmong(
+                        "Phi3-Mini-4K",
+                        "Phi3-Mini-128K"
+                );
+
+            precisionOption = new Option<string>(
+                "--precision",
+                getDefaultValue: () => "INT4",
+                description: "The precision of the model. The only option is INT4.")
+                    .FromAmong(
+                        "INT4"
+                );
+
+            // Create the root command
+            rootCommand = new RootCommand("A CLI app to run model inference.");
+            rootCommand.AddOption(modelOption);
+            rootCommand.AddOption(precisionOption);
+        }
+
+        public async Task<(string, string)> Parse(string[] args)
+        {
+            var requestedModel = string.Empty;
+            var requestedPrecision = string.Empty;
+
+            // Set the handler
+            rootCommand.SetHandler((model, precision) =>
+            {
+                requestedModel = model;
+                requestedPrecision = precision;
+            },
+            modelOption, precisionOption);
+
+            // Invoke the command
+            await rootCommand.InvokeAsync(args);
+
+            // Return the model and precision requested by the user
+            return (requestedModel, requestedPrecision);
+        }
+    }
+}
---
+++ InferenceCLI/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace InferenceCLI
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            // Program variables
+            var status_code = 0;
+            string requestedModel;
+            string requestedPrecision;
+
+            // Parse the CLI
+            var cliParser = new CLI.CLI();
+            (requestedModel, requestedPrecision) = await cliParser.Parse(args);
+
+            Console.WriteLine($"The requested model: {requestedModel}");
+            Console.WriteLine($"The requested precision: {requestedPrecision}");
+
+            // Exit
+            return status_code;
+        }
+    }
+}
